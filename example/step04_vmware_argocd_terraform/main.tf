d8e93e75b780f5e23d250c9765157fcf90c62b7f
kimgura <oli999@naver.com>
2026-07-07 12:32:21 +0900

    step04_vmware_argocd_terraform 예제 추가함

---
+++ example/step04_vmware_argocd_terraform/main.tf
@@ -0,0 +1,92 @@
+# main.tf 파일
+terraform {
+  required_providers {
+    # ArgoCD 전용 프로바이더 선언
+    argocd = {
+      source  = "oboukili/argocd"
+      version = "6.1.1" # 최신 안정화 버전
+    }
+    # terraform 으로 k8s 자원들을 provision 할수 있도록 provider 추가 
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30" 
+    }
+  }
+}
+# 클러스터 접속정보 (local k8s 를 바라 보도록 context 가 변경되어 있어야 한다)
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+# 1. K8s 안에서 ArgoCD Service 리소스를 검색해서 가져옴
+data "kubernetes_service" "argocd_server" {
+  metadata {
+    name      = "argocd-server" # 헬름이 생성한 서비스 이름
+    namespace = "argocd" # namespace 
+  }
+}
+
+provider "argocd" {
+  # cluster_ip (vpn 연결된 eks 에서 사용할 예정)
+  # server_addr = "${data.kubernetes_service.argocd_server.spec[0].cluster_ip}:80" 
+  # external_ip
+  server_addr = "${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].ip}:80"
+
+  # 초기 로그인 계정 정보
+  username    = "admin"
+  password    = "fCJ6hn2BllsHS1z1" 
+
+  # 우리가 --insecure 로 HTTPS를 껐기 때문에 아래 옵션이 반드시 필요합니다!
+  plain_text = true
+  insecure   = true 
+}
+
+# 배포할 app 구성하기
+resource "argocd_application" "member_app"{
+    metadata {
+      name = "member-app"
+      namespace = "argocd"
+    }
+    spec {
+        project = "default"
+        source {
+            # helm repository 주소 또는 github 주소도 가능
+            repo_url = "https://oli999.github.io/chart_test/"
+            # 설치할 chart 의 이름
+            chart = "member-app"
+            # 버전 
+            target_revision = "0.1.1"
+            # (선택) values.yaml 값을 덮어쓰고 싶을 때 사용
+            # helm {
+            #   value_files = ["values.yaml"]
+            #   parameter {
+            #     name  = "replicaCount"
+            #     value = "3"
+            #   }
+            # }
+        }
+        destination {
+            # 정해진 이름 
+            server = "https://kubernetes.default.svc"
+            namespace = "default" # 배포할 namespace 지정 
+        }
+        # 동기화 정책
+        sync_policy {
+            automated {
+                prune       = true
+                self_heal   = true
+            }
+            # namespace 가 없는경우 자동으로 만들어 지도록   
+            sync_options = ["CreateNamespace=true"]
+        }
+    }  
+}
+
+
+
+# external_ip 가 잘 얻어내지는지 확인
+output "argocd_server_ip" {
+  description = "argocd ip"
+  value = "${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].ip}"
+}
+
