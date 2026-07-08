068bc4607cf089f3efb1e2c6dba653bf4330fde0
kimgura <oli999@naver.com>
2026-07-08 11:25:03 +0900

    example/step04 예제 추가함

--- example/step04_vmware_argocd_terraform/assets/memo.md
+++ example/step04_vmware_argocd_terraform/assets/memo.md
@@ -2,3 +2,30 @@
 ![alt text](image.png)
 
 ![alt text](image-1.png)
+
+![alt text](image-2.png)
+
+![alt text](image-3.png)
+
+![alt text](image-4.png)
+
+![alt text](image-5.png)
+
+![alt text](image-6.png)
+
+![alt text](image-7.png)
+
+
+```bash
+# prometheus stack  helm 저장소를 등록하고
+# 1. 프로메테우스 커뮤니티 헬름 레포지토리 등록
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# 2. 메뉴판 최신화 
+helm repo update
+# 3. chart 목록과 version 을 확인한다
+helm search repo prometheus-community
+```
+
+### 목록에서 설치할 chart 의 이름과 version 을 얻어낸다.
+![alt text](image-8.png)
+
--- example/step04_vmware_argocd_terraform/main.tf
+++ example/step04_vmware_argocd_terraform/main.tf
@@ -11,12 +11,48 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.30" 
     }
+    # terraform 으로 helm chart 를 직접 배포 가능하도록 하는 provider 추가
+    helm = {
+      source = "hashicorp/helm"
+      version = "~> 2.14"
+    }
   }
 }
 # 클러스터 접속정보 (local k8s 를 바라 보도록 context 가 변경되어 있어야 한다)
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
+
+# helm provider 가 동작하려면 config 파일 정보를 전달해야 한다. 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+# helm provider 가 동작할 준비가 되어 있으면 "helm_release" 를 사용할수 있다.
+resource "helm_release" "ingress_nginx" {
+  name             = "ingress-nginx"
+  # helm 저장소의 위치
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  # chart 의 이름
+  chart            = "ingress-nginx"
+  # chart 버전
+  version          = "4.11.0"
+  # namespace 설정 
+  namespace        = "ingress-nginx"
+  create_namespace = true
+}
+
+# 테스트로 local 폴더에 있는 ingress rule yaml 파일을 terraform 으로 직접 배포하기
+resource "kubernetes_manifest" "ingress_from_file" {
+  # 주의!  yaml 파일에는 한종류의 deploy, svc 등이 있어야 한다
+  # --- 로 구분해서 여러 종류의 배포는 되지 않는다
+  # namespace 도 반드시 명시해야한다 
+  manifest = yamldecode(file("${path.module}/ingress-rule/rule.yaml"))
+}
+
+
 
 # 1. K8s 안에서 ArgoCD Service 리소스를 검색해서 가져옴
 data "kubernetes_service" "argocd_server" {
@@ -43,8 +79,10 @@ provider "argocd" {
 
 # 배포할 app 구성하기
 resource "argocd_application" "member_app"{
+    # 배포할 app 의 이름과 namespace 를 명시 한다 
     metadata {
       name = "member-app"
+      # argocd 가 거주하고 있는 namespace 를 적어 주어야 한다. 
       namespace = "argocd"
     }
     spec {
@@ -82,7 +120,78 @@ resource "argocd_application" "member_app"{
     }  
 }
 
+# 배포할 app 구성하기
+resource "argocd_application" "microservice_app"{
+    # 배포할 app 의 이름과 namespace 를 명시 한다 
+    metadata {
+      name = "microservice-app"
+      namespace = "argocd"
+    }
+    spec {
+        project = "default"
+        source {
+            # helm repository 주소 또는 github 주소도 가능
+            repo_url = "https://oli999.github.io/helm-microservice/"
+            # 설치할 chart 의 이름
+            chart = "msa-platform"
+            # 버전 
+            target_revision = "0.1.0"
+        }
+        destination {
+            # 정해진 이름 
+            server = "https://kubernetes.default.svc"
+            namespace = "default" # 배포할 namespace 지정 
+        }
+        # 동기화 정책
+        sync_policy {
+            automated {
+                prune       = true
+                self_heal   = true
+            }
+            # namespace 가 없는경우 자동으로 만들어 지도록   
+            sync_options = ["CreateNamespace=true"]
+        }
+    }  
+}
 
+# prometheus stack 을 "argocd_application" 으로 배포할 준비를 해 보세요.
+resource "argocd_application" "promethues_stack"{
+    # 배포할 app 의 이름과 namespace 를 명시 한다 
+    metadata {
+      name = "prometheus-stack"
+      namespace = "argocd"
+    }
+    spec {
+        project = "default"
+        source {
+            # helm repository 주소 또는 github 주소도 가능
+            repo_url = "https://prometheus-community.github.io/helm-charts"
+            # 설치할 chart 의 이름
+            chart = "kube-prometheus-stack"
+            # 버전 
+            target_revision = "87.10.1"
+            # chart 를 설치할때 custom 변수 전달하기
+            helm {
+              values = file("${path.module}/prometheus/my-values.yaml")
+            }
+        }
+        destination {
+            # 정해진 이름 
+            server = "https://kubernetes.default.svc"
+            namespace = "default" # 배포할 namespace 지정 
+        }
+        # 동기화 정책
+        sync_policy {
+            automated {
+                prune       = true
+                self_heal   = true
+            }
+            # 크기가 크고 무거운 chart 는 ServerSideApply=true 옵션을 같이 전달한다
+            # argocd 는 용량 제한이 있기때문에 k8s 에 직접 던져서 실행이 되도록 
+            sync_options = ["CreateNamespace=true", "ServerSideApply=true"]
+        }
+    }  
+}
 
 # external_ip 가 잘 얻어내지는지 확인
 output "argocd_server_ip" {
