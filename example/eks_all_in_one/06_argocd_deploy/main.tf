45798895921bfca168ad42abd33c56d5c734ae11
kimgura <oli999@naver.com>
2026-07-10 11:08:29 +0900

    수정함

--- example/eks_all_in_one/06_argocd_deploy/main.tf
+++ example/eks_all_in_one/06_argocd_deploy/main.tf
@@ -80,57 +80,57 @@ provider "argocd" {
 # }
 
 # prometheus stack 을 "argocd_application" 으로 배포할 준비를 해 보세요.
-resource "argocd_application" "promethues_stack"{
-    # 배포할 app 의 이름과 namespace 를 명시 한다 
-    metadata {
-      name = "prometheus-stack"
-      namespace = "argocd"
-    }
-    spec {
-        project = "default"
-        source {
-            # helm repository 주소 또는 github 주소도 가능
-            repo_url = "https://prometheus-community.github.io/helm-charts"
-            # 설치할 chart 의 이름
-            chart = "kube-prometheus-stack"
-            # 최신 버전 
-            target_revision = "87.10.1"
-            # chart 를 설치할때 custom 변수 전달하기
-            helm {
-              values = file("${path.module}/prometheus/my-values.yaml")
-            }
-        }
-        destination {
-            # 정해진 이름 
-            server = "https://kubernetes.default.svc"
-            namespace = "prometheus" # 배포할 namespace 지정 
-        }
-        # 동기화 정책
-        sync_policy {
-            automated {
-                prune       = true
-                self_heal   = true
-            }
-            # 크기가 크고 무거운 chart 는 ServerSideApply=true 옵션을 같이 전달한다
-            # argocd 는 용량 제한이 있기때문에 k8s 에 직접 던져서 실행이 되도록 
-            sync_options = ["CreateNamespace=true", "ServerSideApply=true"]
-        }
-        # 쿠버네티스가 자동으로 주입하는 인증서 값 무시하기 (무한 핑퐁 방지)
-        ignore_difference {
-          group         = "admissionregistration.k8s.io"
-          kind          = "ValidatingWebhookConfiguration"
-          name          = "prometheus-stack-kube-prom-admission"
-          json_pointers = ["/webhooks/0/clientConfig/caBundle"]
-        }
+# resource "argocd_application" "promethues_stack"{
+#     # 배포할 app 의 이름과 namespace 를 명시 한다 
+#     metadata {
+#       name = "prometheus-stack"
+#       namespace = "argocd"
+#     }
+#     spec {
+#         project = "default"
+#         source {
+#             # helm repository 주소 또는 github 주소도 가능
+#             repo_url = "https://prometheus-community.github.io/helm-charts"
+#             # 설치할 chart 의 이름
+#             chart = "kube-prometheus-stack"
+#             # 최신 버전 
+#             target_revision = "87.10.1"
+#             # chart 를 설치할때 custom 변수 전달하기
+#             helm {
+#               values = file("${path.module}/prometheus/my-values.yaml")
+#             }
+#         }
+#         destination {
+#             # 정해진 이름 
+#             server = "https://kubernetes.default.svc"
+#             namespace = "prometheus" # 배포할 namespace 지정 
+#         }
+#         # 동기화 정책
+#         sync_policy {
+#             automated {
+#                 prune       = true
+#                 self_heal   = true
+#             }
+#             # 크기가 크고 무거운 chart 는 ServerSideApply=true 옵션을 같이 전달한다
+#             # argocd 는 용량 제한이 있기때문에 k8s 에 직접 던져서 실행이 되도록 
+#             sync_options = ["CreateNamespace=true", "ServerSideApply=true"]
+#         }
+#         # 쿠버네티스가 자동으로 주입하는 인증서 값 무시하기 (무한 핑퐁 방지)
+#         ignore_difference {
+#           group         = "admissionregistration.k8s.io"
+#           kind          = "ValidatingWebhookConfiguration"
+#           name          = "prometheus-stack-kube-prom-admission"
+#           json_pointers = ["/webhooks/0/clientConfig/caBundle"]
+#         }
 
-        ignore_difference {
-          group         = "admissionregistration.k8s.io"
-          kind          = "MutatingWebhookConfiguration"
-          name          = "prometheus-stack-kube-prom-admission"
-          json_pointers = ["/webhooks/0/clientConfig/caBundle"]
-        }
-    }  
-}
+#         ignore_difference {
+#           group         = "admissionregistration.k8s.io"
+#           kind          = "MutatingWebhookConfiguration"
+#           name          = "prometheus-stack-kube-prom-admission"
+#           json_pointers = ["/webhooks/0/clientConfig/caBundle"]
+#         }
+#     }  
+# }
 
 # external_ip 가 잘 얻어내지는지 확인
 output "argocd_server_ip" {
@@ -138,3 +138,87 @@ output "argocd_server_ip" {
   value = "${data.kubernetes_service.argocd_server.spec[0].cluster_ip}"
 }
 
+
+resource "argocd_application" "postgres-app" {
+  metadata {
+    name      = "postgres-app"
+    namespace = "argocd"
+  }
+  spec {
+    project = "default"
+    source {
+      # 1. 일반 Git 저장소 주소
+      repo_url        = "https://github.com/oli999/argocd_deploy.git"
+      target_revision = "master"
+      
+      # 2. Chart.yaml이 들어있는 폴더 경로 지정
+      path            = "postgres" 
+      
+      # 3. 폴더가 Helm 구조라면 helm 블록을 통해 값을 제어할 수 있습니다!
+      # helm {
+      #   # Git 폴더 안에 있는 특정 values 파일을 덮어쓰고 싶을 때
+      #   value_files = ["values-prod.yaml"] 
+        
+      #   # 또는 개별 변수를 직접 꽂아 넣고 싶을 때
+      #   parameter {
+      #     name  = "image.tag"
+      #     value = "v1.0.5"
+      #   }
+      # }
+    }
+    
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "default" 
+    }
+    sync_policy {
+      automated {
+        prune       = true
+        self_heal   = true
+      }
+      sync_options = ["CreateNamespace=true"]
+    }
+  }  
+}
+
+resource "argocd_application" "microservice-app" {
+  metadata {
+    name      = "microservice-app"
+    namespace = "argocd"
+  }
+  spec {
+    project = "default"
+    source {
+      # 1. 일반 Git 저장소 주소
+      repo_url        = "https://github.com/oli999/argocd_deploy.git"
+      target_revision = "master"
+      
+      # 2. Chart.yaml이 들어있는 폴더 경로 지정
+      path            = "microservice" 
+      
+      # 3. 폴더가 Helm 구조라면 helm 블록을 통해 값을 제어할 수 있습니다!
+      # helm {
+      #   # Git 폴더 안에 있는 특정 values 파일을 덮어쓰고 싶을 때
+      #   value_files = ["values-prod.yaml"] 
+        
+      #   # 또는 개별 변수를 직접 꽂아 넣고 싶을 때
+      #   parameter {
+      #     name  = "image.tag"
+      #     value = "v1.0.5"
+      #   }
+      # }
+    }
+    
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "default" 
+    }
+    sync_policy {
+      automated {
+        prune       = true
+        self_heal   = true
+      }
+      sync_options = ["CreateNamespace=true"]
+    }
+  }  
+}
